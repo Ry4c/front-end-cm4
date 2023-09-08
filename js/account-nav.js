1d84d6823e273b2fb43fd69f176f07271e13b591
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-09-08 10:35:29 +0700

    pre done navbar

--- js/account-nav.js
+++ js/account-nav.js
@@ -1,7 +1,27 @@
 $(window).on('load', function (){
-    if(localStorage.getItem('accData')) $('#userName').html(JSON.parse(localStorage.getItem('accData')).name)
-    else $('#userName').html(Login);
-});
+        if(localStorage.getItem('accData')){
+            $('#userName').html(JSON.parse(localStorage.getItem('accData')).name);
+            $('#acc-btn').attr("title",'Logout').click(function (){
+                localStorage.removeItem('accData');
+            })
+        } else {
+            $('#userName').html("Login");
+            $('#acc-btn').attr("title",'Login').click(function (){
+                window.location="./login.html";
+            });
+
+        }
+    },
+);
+$(window).on('load',function (){
+    let genresList = JSON.parse(localStorage.getItem("genres"));
+    let str='';
+    for (const genres of genresList) {
+        str += `<li><a href="./categories/${genres}">${genres}</a></li>`
+    }
+    $('#genresList').html(str)
+})
+
 $('#acc-btn').click(function (){
     if(localStorage.getItem('accData')){
         localStorage.removeItem('accData');
