dd653c96cadf47c185dddc95d03506bdf960afdf
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-09-08 08:56:07 +0700

    add account-search file

---
+++ js/account-search.js
@@ -0,0 +1,30 @@
+$(window).on('load', function (){
+    if(localStorage.getItem('accData')) $('#userName').html(JSON.parse(localStorage.getItem('accData')).name)
+});
+$('#acc-btn').click(function (){
+    if(localStorage.getItem('accData')){
+        localStorage.removeItem('accData');
+        window.location ='./anime-details.html';
+    }else {
+        window.location ='./login.html';
+        $('#acc-btn').attr('title','Login')
+    }
+})
+$('#search-input').on('keypress',function(e) {
+    if(e.which === 13) {
+        $.ajax({
+            type: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type':'application/json'
+            },
+            url: "http://localhost:8080/students"+$('#search-input').val(),
+            success: function (data) {
+                //     show search result
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        });
+    }
+})
