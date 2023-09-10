0baf278c90f2fa3469d9b8e240ecbab02b05d9ce
Ry4c <131599009+Ry4c@users.noreply.github.com>
2023-09-10 14:36:16 +0700

    finish header

--- js/account-nav.js
+++ js/account-nav.js
@@ -13,6 +13,11 @@ $(window).on('load', function (){
         }
     }
 );
+$(window).on('load',function (){
+    if(!localStorage.getItem('accData')){$('#add-str').hide();
+    } else if(JSON.parse(localStorage.getItem('accData')).roles[0].id === 1) $('#add-str').show()
+})
+
 $('#acc-btn').click(function (){
     if(localStorage.getItem('accData')){
         localStorage.removeItem('accData');
@@ -24,34 +29,63 @@ $('#acc-btn').click(function (){
 })
 
 $(window).on('load',function (){
-    $.ajax({
-        type: "GET",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type':'application/json'
-        },
-        url: "http://localhost:8080/genres",
-        success: function (data) {
-            localStorage.setItem("genres",JSON.stringify(data));
-        },
-        error: function (err) {
-            console.log(err)
-        }
-    });
+    if (!localStorage.getItem('genres')){
+        $.ajax({
+            type: "GET",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type':'application/json'
+            },
+            url: "http://localhost:8080/search/genres",
+            success: function (data) {
+                localStorage.setItem("genres",JSON.stringify(data));
+            },
+            error: function (err) {
+                console.log(err)
+            }
+        });
+    }
+    showGenres();
 });
 
-$(window).on('load',function (){
+function showGenres(){
     let genresList = JSON.parse(localStorage.getItem("genres"));
     let str='';
     for (const genres of genresList) {
-        str += `<li><a href="./categories/${genres}">${genres}</a></li>`
+        str += `<li><a href="./categories.html/?genre=${genres.name}">${genres.name}</a></li>`
     }
     $('#genresList').html(str)
-});
+}
 
 $('#search-input').on('keypress',function(e) {
     if(e.which === 13) {
         localStorage.setItem("search",$('#search-input').val());
         window.location="./index.html"
     }
+})
+
+$('#addStory').click(function (){
+    let title = $("#title-add").val();
+    let img = $("#img-add").val();
+    let description = $("#description-add").val();
+    let status = $("#status-add").val();
+    let vote = $("#votes-add").val();
+    let rating = $("#rating-add").val();
+    let story = {title,img,description,status,vote,rating}
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type':'application/json',
+            "Authorization":"Bearer " + JSON.parse(localStorage.getItem('accData')).token
+        },
+        url: "http://localhost:8080/stories/create",
+        data: JSON.stringify(story),
+        success: function (data) {
+            alert("Succeed")
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    });
 })
